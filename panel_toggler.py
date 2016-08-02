117ce66310e15c8128aa3464a7f08d7af5beed4e
jonian <jonian@hardpixel.eu>
2016-08-02 16:04:10 +0300

    refactoring

--- panel_toggler.py
+++ panel_toggler.py
@@ -1,53 +1,76 @@
-from gi.repository import GObject, Gtk, Gedit, Gdk
-
 import os
+import gi
 
-class CutLineWindowActivatable(GObject.Object, Gedit.WindowActivatable):
+gi.require_version('Gtk', '3.0')
+gi.require_version('Gedit', '3.0')
+
+from gi.repository import GObject, Gtk, Gedit
+
+
+class PanelTogglerWindowActivatable(GObject.Object, Gedit.WindowActivatable):
 
 	window = GObject.property(type=Gedit.Window)
 
 	def __init__(self):
 		GObject.Object.__init__(self)
 
+		self._header_bar    = None
+		self._panel_sidebar = None
+		self._left_button   = None
+		self._bottom_button = None
+
 	def do_activate(self):
-		path = os.path.dirname(__file__)
-		hbar = self.find_widget_by_id(self.window, "headerbar")
+		self._header_bar    = find_widget(self.window, "headerbar")
+		self._panel_sidebar = find_widget(self.window, "bottom_panel_sidebar")
+		self._left_button   = Gtk.Button(image=image_file("left"))
+		self._bottom_button = Gtk.Button(image=image_file("bottom"))
 
-		image = Gtk.Image.new_from_file(path + "/icons/gedit-view-bottom-pane-symbolic.svg")
-		self.bottom_button = Gtk.Button(image=image)
-		self.bottom_button.connect('clicked', self.on_bottom_pane_button_activated)
-		self.bottom_button.show()
-		hbar.pack_end(self.bottom_button)
+		self._bottom_button.connect("clicked", self.on_bottom_button_activated)
+		self._left_button.connect("clicked", self.on_left_button_activated)
 
-		image = Gtk.Image.new_from_file(path + "/icons/gedit-view-left-pane-symbolic.svg")
-		self.left_button = Gtk.Button(image=image)
-		self.left_button.connect('clicked', self.on_left_pane_button_activated)
-		self.left_button.show()
-		hbar.pack_end(self.left_button)
+		self._header_bar.pack_end(self._bottom_button)
+		self._header_bar.pack_end(self._left_button)
 
-		self.panel_sidebar = self.find_widget_by_id(self.window, "bottom_panel_sidebar")
-		self.panel_sidebar.hide()
+		self._left_button.show()
+		self._bottom_button.show()
+		self._panel_sidebar.hide()
 
 	def do_deactivate(self):
-		self.left_button.destroy()
-		self.bottom_button.destroy()
-		self.panel_sidebar.show()
+		self._left_button.destroy()
+		self._bottom_button.destroy()
+		self._panel_sidebar.show()
 
-	def on_left_pane_button_activated(self, action=None, user_data=None):
+	def on_left_button_activated(self, _button):
 		panel = self.window.get_side_panel()
 		status = not panel.get_property("visible")
+
 		panel.set_property("visible", status)
 
-	def on_bottom_pane_button_activated(self, action=None, user_data=None):
+	def on_bottom_button_activated(self, _button):
 		panel = self.window.get_bottom_panel()
 		status = not panel.get_property("visible")
+
 		panel.set_property("visible", status)
 
-	def find_widget_by_id(self, widget, id):
-		if Gtk.Buildable.get_name(widget) == id: return widget
-		if not hasattr(widget, "get_children"): return None
 
-		for child in widget.get_children():
-			ret = self.find_widget_by_id(child, id)
-			if ret: return ret
+def find_widget(widget, widget_id):
+	if Gtk.Buildable.get_name(widget) == widget_id:
+		return widget
+
+	if not hasattr(widget, "get_children"):
 		return None
+
+	for child in widget.get_children():
+		ret = find_widget(child, widget_id)
+
+		if ret:
+			return ret
+
+	return None
+
+
+def image_file(image):
+	path = os.path.dirname(__file__)
+	icon = "/icons/gedit-view-" + image + "-pane-symbolic.svg"
+
+	return Gtk.Image.new_from_file(path + icon)
