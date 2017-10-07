fc7f87001f8d358b0cc0a2495255c48e7b73e9f1
Jonian Guveli <jonian@hardpixel.eu>
2017-10-07 05:07:42 +0300

    convert indentation to spaces

--- panel_toggler.py
+++ panel_toggler.py
@@ -9,58 +9,58 @@ from gi.repository import GObject, Gtk, Gedit
 
 class PanelTogglerWindowActivatable(GObject.Object, Gedit.WindowActivatable):
 
-	window = GObject.property(type=Gedit.Window)
+  window = GObject.property(type=Gedit.Window)
 
-	def __init__(self):
-		GObject.Object.__init__(self)
+  def __init__(self):
+    GObject.Object.__init__(self)
 
-		self._header_bar    = None
-		self._bottom_panel  = None
-		self._side_panel    = None
-		self._panel_sidebar = None
-		self._left_button   = None
-		self._bottom_button = None
+    self._header_bar    = None
+    self._bottom_panel  = None
+    self._side_panel    = None
+    self._panel_sidebar = None
+    self._left_button   = None
+    self._bottom_button = None
 
-	def do_activate(self):
-		self.add_icon_path()
+  def do_activate(self):
+    self.add_icon_path()
 
-		self._header_bar    = self.window.get_titlebar().get_children()[-1]
-		self._bottom_panel  = self.window.get_bottom_panel()
-		self._side_panel    = self.window.get_side_panel()
-		self._panel_sidebar = self._bottom_panel.get_parent().get_parent().get_children()[-1]
-		self._button_box    = Gtk.Box()
-		self._left_button   = Gtk.Button.new_from_icon_name("left-panel-symbolic", Gtk.IconSize.BUTTON)
-		self._bottom_button = Gtk.Button.new_from_icon_name("bottom-panel-symbolic", Gtk.IconSize.BUTTON)
+    self._header_bar    = self.window.get_titlebar().get_children()[-1]
+    self._bottom_panel  = self.window.get_bottom_panel()
+    self._side_panel    = self.window.get_side_panel()
+    self._panel_sidebar = self._bottom_panel.get_parent().get_parent().get_children()[-1]
+    self._button_box    = Gtk.Box()
+    self._left_button   = Gtk.Button.new_from_icon_name("left-panel-symbolic", Gtk.IconSize.BUTTON)
+    self._bottom_button = Gtk.Button.new_from_icon_name("bottom-panel-symbolic", Gtk.IconSize.BUTTON)
 
-		self._button_box.get_style_context().add_class("linked")
-		self._bottom_button.connect("clicked", self.on_bottom_button_activated)
-		self._left_button.connect("clicked", self.on_left_button_activated)
+    self._button_box.get_style_context().add_class("linked")
+    self._bottom_button.connect("clicked", self.on_bottom_button_activated)
+    self._left_button.connect("clicked", self.on_left_button_activated)
 
-		self._header_bar.pack_end(self._button_box)
-		self._button_box.pack_end(self._bottom_button, True, True, 0)
-		self._button_box.pack_end(self._left_button, True, True, 1)
+    self._header_bar.pack_end(self._button_box)
+    self._button_box.pack_end(self._bottom_button, True, True, 0)
+    self._button_box.pack_end(self._left_button, True, True, 1)
 
-		self._button_box.show()
-		self._left_button.show()
-		self._bottom_button.show()
-		self._panel_sidebar.hide()
+    self._button_box.show()
+    self._left_button.show()
+    self._bottom_button.show()
+    self._panel_sidebar.hide()
 
-	def do_deactivate(self):
-		self._left_button.destroy()
-		self._bottom_button.destroy()
-		self._button_box.destroy()
-		self._panel_sidebar.show()
+  def do_deactivate(self):
+    self._left_button.destroy()
+    self._bottom_button.destroy()
+    self._button_box.destroy()
+    self._panel_sidebar.show()
 
-	def on_left_button_activated(self, _button):
-		status = not self._side_panel.get_property("visible")
-		self._side_panel.set_property("visible", status)
+  def on_left_button_activated(self, _button):
+    status = not self._side_panel.get_property("visible")
+    self._side_panel.set_property("visible", status)
 
-	def on_bottom_button_activated(self, _button):
-		status = not self._bottom_panel.get_property("visible")
-		self._bottom_panel.set_property("visible", status)
+  def on_bottom_button_activated(self, _button):
+    status = not self._bottom_panel.get_property("visible")
+    self._bottom_panel.set_property("visible", status)
 
-	def add_icon_path(self):
-		theme = Gtk.IconTheme.get_default()
-		path  = "%s/icons" % os.path.dirname(__file__)
+  def add_icon_path(self):
+    theme = Gtk.IconTheme.get_default()
+    path  = "%s/icons" % os.path.dirname(__file__)
 
-		Gtk.IconTheme.append_search_path(theme, path)
+    Gtk.IconTheme.append_search_path(theme, path)
