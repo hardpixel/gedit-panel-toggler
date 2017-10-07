b514026039c4f7e46c8d4c806452267859444335
Jonian Guveli <jonian@hardpixel.eu>
2017-10-07 05:30:09 +0300

    replace double-quotes with single-quotes

--- panel_toggler.py
+++ panel_toggler.py
@@ -29,12 +29,12 @@ class PanelTogglerWindowActivatable(GObject.Object, Gedit.WindowActivatable):
     self._side_panel    = self.window.get_side_panel()
     self._panel_sidebar = self._bottom_panel.get_parent().get_parent().get_children()[-1]
     self._button_box    = Gtk.Box()
-    self._left_button   = Gtk.Button.new_from_icon_name("left-panel-symbolic", Gtk.IconSize.BUTTON)
-    self._bottom_button = Gtk.Button.new_from_icon_name("bottom-panel-symbolic", Gtk.IconSize.BUTTON)
+    self._left_button   = Gtk.Button.new_from_icon_name('left-panel-symbolic', Gtk.IconSize.BUTTON)
+    self._bottom_button = Gtk.Button.new_from_icon_name('bottom-panel-symbolic', Gtk.IconSize.BUTTON)
 
-    self._button_box.get_style_context().add_class("linked")
-    self._bottom_button.connect("clicked", self.on_bottom_button_activated)
-    self._left_button.connect("clicked", self.on_left_button_activated)
+    self._button_box.get_style_context().add_class('linked')
+    self._bottom_button.connect('clicked', self.on_bottom_button_activated)
+    self._left_button.connect('clicked', self.on_left_button_activated)
 
     self._header_bar.pack_end(self._button_box)
     self._button_box.pack_end(self._bottom_button, True, True, 0)
@@ -58,9 +58,9 @@ class PanelTogglerWindowActivatable(GObject.Object, Gedit.WindowActivatable):
     self._panel_sidebar.show()
 
   def on_left_button_activated(self, _button):
-    status = not self._side_panel.get_property("visible")
-    self._side_panel.set_property("visible", status)
+    status = not self._side_panel.get_property('visible')
+    self._side_panel.set_property('visible', status)
 
   def on_bottom_button_activated(self, _button):
-    status = not self._bottom_panel.get_property("visible")
-    self._bottom_panel.set_property("visible", status)
+    status = not self._bottom_panel.get_property('visible')
+    self._bottom_panel.set_property('visible', status)
